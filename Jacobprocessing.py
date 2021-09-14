f548a119028cdc85e242276721963e7c278bb35f
YoavTarazy <60843819+YoavTarazy@users.noreply.github.com>
2021-09-14 21:35:50 +0300

    innit repo

---
+++ Jacobprocessing.py
@@ -0,0 +1,268 @@
+from matplotlib import colors
+import numpy as np
+import cv2 as cv2
+from numpy.core.fromnumeric import ndim, shape
+import scipy.ndimage as simg
+from scipy import misc
+from PIL import Image
+import matplotlib.pyplot as plt
+import math
+import json
+import time
+from colour import Color
+import requests
+import random
+
+
+#image loader + turns all colors beside white to black.
+def imageload_blacklines(img):
+    pic=Image.open(img)
+    red=[]
+    pix=np.array(pic)
+    h,w,bpp =pix.shape
+    for py in range(h):
+        for px in range(w):
+            p=pix[py][px]
+            #change to np.where and define specific pixels, Combine findredpixels to this function
+            if(p[0]!=255 and p[1]!=255 and p[2]!=255 ):
+                   if (pix[py][px][1]!=0 and pix[py][px][2]!=0):
+                      p[0],p[1],p[2]=0,0,0
+            elif (p[0]==255 and p[1]==0 and p[2]==0):
+                red.append((py,px))
+    
+    #Saves red values in a Json                      
+    with open('reddots2.json','w') as f:
+        json.dump(red,f,ensure_ascii=False, indent=4)  
+                   
+        
+    pic2=Image.fromarray(pix)
+    pic2.save("blackendpic.png")
+
+#Fills a Json with an array that matches the image but contains distances from closest center              
+def distances_to_Json(pix,reddots):
+    h,w,rgb=pix.shape
+    distances=np.zeros((h,w),dtype=float)
+    for py in range(h):
+        for px in range(w):
+            t=1000000000
+            for dot in reddots:
+                if(math.dist((py,px),dot)<t):
+                    t=math.dist((py,px),dot)
+                    distances[py][px]=int(t)
+            
+            
+            
+    with open('distances.json','w') as f:
+        json.dump(distances.tolist(),f,ensure_ascii=False, indent=4)                
+
+#crops the image according to black edge around plot given earlier. DEPRACATED
+def image_cropper(img):
+    im=Image.open(img)
+    blacklist=[]
+    pix=np.asarray(im)
+    h,w,rgb=pix.shape
+    for py in range(h):
+        for px in range(w):
+            if is_pixel_color(pix[py][px],py,px,[0,0,0]):
+                blacklist.append((py,px))
+    unzipped_blacklist=list(zip(*blacklist))
+    
+    min_width=min(unzipped_blacklist[1])
+    max_width=max(unzipped_blacklist[1])
+    min_height=min(unzipped_blacklist[0])
+    max_height=max(unzipped_blacklist[0])
+    im2=im.crop(box=(min_width,min_height,max_width,max_height))
+    im2.save('croppedimage.png')        
+                 
+#Checks color of specific pixel in the image.   
+def is_pixel_color(pixel,colorRGB):
+    if(pixel[0]==colorRGB[0] and pixel[1]==colorRGB[1] and pixel[2]==colorRGB[2]):
+        return True
+    return False
+
+
+    
+#recieves a starting color and an end color and returns a tuple of rgb values that are the gradient from start to end.        
+def color_gradient_section(s_color,e_color,steps):
+    
+    red=Color(s_color)
+    colors=list(red.range_to(Color(e_color),steps))
+    for c in range(steps):
+        colors[c]=colors[c].rgb
+        colors[c]=(int(255*colors[c][0]),int(255*colors[c][1]),int(255*colors[c][2]))
+    return colors
+
+#This function colors one pixel from the image
+#def color_a_pixel(pixel_rgb,coordinates,white_pallete,black_pallete):
+    #if is_pixel_color()
+    
+    
+#This function calculates from the distance of the pixel from the closest center, the color it needs to be painted.
+def calculate_coloring_section(pix,distances,segment,sub_segment,colors_white,colors_black):
+   h,w=distances.shape
+   
+   for py in range(h):
+       for px in range(w):
+           dis=distances[py][px]
+           floor=int(dis/segment)
+           step=round((dis%segment)/sub_segment)
+           if is_pixel_color(pix[py][px],(255,255,255)):
+               pix[py][px][0],pix[py][px][1],pix[py][px][2]=colors_white[min(3,floor)][min(9,step)][0],colors_white[min(3,floor)][min(9,step)][1],colors_white[min(3,floor)][min(9,step)][2]
+           elif is_pixel_color(pix[py][px],(0,0,0)):
+               pix[py][px][0],pix[py][px][1],pix[py][px][2]=colors_black[min(3,floor)][min(9,step)][0],colors_black[min(3,floor)][min(9,step)][1],colors_black[min(3,floor)][min(9,step)][2]
+               
+   img=Image.fromarray(pix)
+   try:
+    img.save(f'D:/Programing/Magnetic Emotion/Ampere_Genesis/Pictures/Impressions/Impression_1_floorsize_{segment}_stepsize_{sub_segment}.png'.format(segment=floor,sub_segment=step))
+   except IOError:
+       print("couldnt save in specific location")
+       img.save('coloredpic.png')             
+    
+#this function recieves an n-tuple list of colors (strings) and converts them to the gradientts between them                
+def create_color_palletes(colors,steps):
+    pallet=[]
+    fcolor=colors[0]
+    for color in colors[1:]:
+        pallet.append(color_gradient_section(fcolor,color,steps))
+        fcolor=color
+    return pallet    
+
+#This function recieves the array for the image, the distance json, the max distance from it,and the number of colors to gradient and how many gradient stages requested.
+def color_tiles(pix,distances,floors,steps,color_lst):
+    
+    #calculated manualy from left center to (0,0) to acheive biggest minimal distance possible.
+    max_distance=2705
+    
+    #sectioning the image accordingly
+    floorsize=max_distance/(floors) #900
+    stepsize=floorsize/steps #300
+    
+    #creating color palletes
+    pallete_for_blacklines=create_color_palletes(random_color_pallete(color_lst,floors),steps)
+    pallete_for_whitebg=create_color_palletes(random_color_pallete(color_lst,floors),steps)
+    
+    #The coloring of the image algorithm
+    h,w=distances.shape
+    for py in range(h):
+           for px in range(w):
+            dis=distances[py][px]
+            floor=math.ceil(dis/floorsize) #2
+            real_floor=floor-1
+            step=math.ceil((dis-(floorsize*floor))/stepsize)
+            p=pix[py][px]
+            if is_pixel_color(pix[py][px],(255,255,255)):
+               try:   
+                p[0],p[1],p[2]=pallete_for_blacklines[real_floor][step][0],pallete_for_blacklines[real_floor][step][1],pallete_for_blacklines[real_floor][step][2]
+               except IndexError:
+                   print("error in: coordinates- ({y},{x}), distance- {dis}, floor- {floor}, steps- {step}".format(y=py,x=px,dis=dis,floor=floor,step=step))
+                   
+            else:
+                try: 
+                    p[0],p[1],p[2]=pallete_for_whitebg[real_floor][step][0],pallete_for_whitebg[real_floor][step][1],pallete_for_whitebg[real_floor][step][2]
+                except IndexError:
+                    print("error in: coordinates- ({y},{x}), distance- {dis}, floor- {floor}, steps- {step}".format(y=py,x=px,dis=dis,floor=floor,step=step))
+                
+               
+    img=Image.fromarray(pix)
+    try:
+        img.save('D:/Programing/Magnetic Emotion/Ampere_Genesis/Pictures/Impressions/Impression_floorsize_{segment}_stepsize_{sub_segment}.png'.format(segment=floors,sub_segment=steps))
+    except IOError:
+       print("couldnt save in specific location")
+       img.save('coloredpic.png')  
+        
+#pulls all color names from github    
+def pull_colours():
+    url ='https://gist.githubusercontent.com/rortian/7516084/raw/1834f5f6475b74e18c05814f8e8441aa5b2f9adc/svg-named-colors.json'
+    resp=requests.get(url)
+    dic=json.loads(resp.text)
+    print(dic)
+    return dic               
+
+#randomizes a color list
+def random_color_pallete(overall_color_list,size_of_pallete):
+    pallete=[]
+    for c in range(size_of_pallete+1):
+        try:
+            i=random.randint(0,len(overall_color_list)-1)
+            pallete.append(overall_color_list[i])
+        except ValueError:
+            pallete.pop(i)
+            c=c-1
+    return pallete
+
+#pulls gradient list from original pic and saves it to Json
+def line_pallete_toJson(pix):
+    h,w,rgb=pix.shape
+    colors=np.unique(pix.reshape(-1, pix.shape[2]), axis=0)
+    colors=colors.tolist()
+    colors.remove([255,255,255,255])
+    print(colors)
+    with open('colors of lines.json','w') as f:
+        json.dump(colors,f)               
+
+#Creates the gradient of the lines
+def gradient_for_lines(line_colors,overall_color_list):
+    floors=1
+    steps_between_each_floor=len(line_colors)-floors
+    floor_colors=random_color_pallete(overall_color_list,floors)
+    color_steps=create_color_palletes(floor_colors,steps_between_each_floor)
+       
+    print(len(line_colors))
+    print(len(color_steps))
+    return color_steps
+    
+        
+        
+        
+    
+    
+def color_lines(pix,line_colors,overall_color_list):
+    new_line_colors=gradient_for_lines(line_colors,overall_color_list)[0]
+    h,w,rgb=pix.shape
+    for py in range(h):
+        for px in range(w):
+            p=pix[py][px]
+            k=line_colors.index((p[0],p[1],p[2],p[3]))
+            if not is_pixel_color(p,(255,255,255,255)):
+                
+                try:
+                    k=line_colors.index((p[0],p[1],p[2],p[3]))
+                    print(k)
+                    p[0],p[1],p[2]=new_line_colors[k][0],new_line_colors[k][1],new_line_colors[k][2]
+                except ValueError:
+                   print("error in, ",p) 
+                
+    img=Image.fromarray(pix)
+    img.save('onlystripes.png')
+    print('done')            
+            
+    
+    
+               
+
+def placeholder():        
+    with open('distances.json') as f:
+        dist=np.array(json.load(f))
+        
+    pix=np.array(Image.open('blackendpic.png'))
+
+    colorlist=pull_colours()    
+    colors_white=create_color_palletes(('white','yellow','orange','red','blue'))
+    colors_black=create_color_palletes(('white','yellow','red','cornflowerblue','darkblue'))
+    calculate_coloring_section(pix,dist,500,50,colors_white,colors_black)
+    print(random_color_pallete(pull_colours(),5))
+
+with open('distances.json') as f:
+        dist=np.array(json.load(f))
+        
+pix=np.array(Image.open('D:/Programing/Magnetic Emotion/Ampere_Genesis/Pictures/Universe/New/NewAmpereT_17_c1_vert_x400z10_ls1000stps2000_figsz_w40h25dpi300_density_15_linewidth4.png'))
+h,w,rgb=pix.shape
+print(rgb)
+line_pallete_toJson(pix)
+
+with open('colors of lines.json') as f:
+    line_colors=json.load(f)
+            
+
+
+color_lines(pix,line_colors,pull_colours())
---
+++ Magneticfieldcalculator.py
@@ -0,0 +1,67 @@
+import PIL
+import magpylib as mag3
+from magpylib import current
+from magpylib._lib.obj_classes.class_Collection import Collection
+import matplotlib
+import matplotlib.patches as patches
+import matplotlib.pyplot as plt
+import numpy as np
+from PIL import Image
+import cv2 as cv
+import imageio
+from numpy.lib.arraypad import pad
+
+
+
+def m_field_of_current():
+    
+    #Creating The Magnets
+    c=1
+    vx,vz=400,10
+    y1=mag3.current.Line(current=1,vertices=[(-400,0,-10),(-400,0,10)])
+    y2=mag3.current.Line(current=-1,vertices=[(400,0,-10),(400,0,10)])
+    Rec1= patches.Rectangle((-400,0),1,0,color='red')
+    Rec2= patches.Rectangle((400,0),1,0,color='red')
+    
+    
+    #Creating the collection
+    c2=Collection(y1,y2)
+    
+
+    
+    #Building the UI
+    ##Building the space to present the calculations on
+    ls=1000
+    steps=2000
+    xs=np.linspace(-ls,ls,steps)
+    ys=np.linspace(-ls,ls,steps)
+    
+    ##Performing the calculations
+    POS=np.array([(x,y,0) for y in ys for x in xs])
+    Bs = c2.getB(POS).reshape(steps ,steps,-1)
+    
+    ##Create the figure that holds the streamplot
+    w=40
+    h=25
+    dpi=300
+    fig=plt.figure(figsize=(w,h),dpi=dpi,frameon=True)
+    
+    #Creating and painting the subplot
+    ax2=fig.add_subplot()
+    X,Z=np.meshgrid(xs,ys)
+    U,V=Bs[:,:,0], Bs[:,:,1]
+    dens=15
+    lw=4
+    ax2.streamplot(X,Z,U,V,color=np.log(U**16+V**16),density=dens,arrowsize=0,linewidth=lw)
+    matplotlib.rc('axes',edgecolor='green')
+    ax2.add_patch(Rec1)
+    ax2.add_patch(Rec2)
+    plt.axis('off')
+    plt.savefig('D:/Programing/Magnetic Emotion/Ampere_Genesis/Pictures/Universe/New/NewAmpereT_17_c{c}_vert_x{vx}z{vz}_ls{ls}stps{steps}_figsz_w{w}h{h}dpi{dpi}_density_{dens}_linewidth{lwid}.png'.format(c=c,vx=vx,vz=vz,ls=ls,steps=steps,w=w,h=h,dpi=dpi,dens=dens,lwid=lw),transparent=False,edgecolor=fig.get_edgecolor(),bbox_inches='tight')
+    plt.tight_layout()
+    print("Done")
+    
+
+Image.MAX_IMAGE_PIXELS=500000000
+#print(checkifsymmetrical('Try.png'))
+m_field_of_current()
