52e43b4c49fe5342040bb134eb78d0ad8cd39da4
YoavTarazy <60843819+YoavTarazy@users.noreply.github.com>
2021-09-16 16:34:28 +0300

    added-looping-math
    this will create painting changing via the formula for colour

--- Main_operation.py
+++ Main_operation.py
@@ -3,7 +3,7 @@ import json
 import numpy as np
 import Magneticfieldcalculator
 
-
+    
 
 def run_multi_universe():
     
@@ -12,11 +12,12 @@ def run_multi_universe():
     with open('./json/cmaps.json') as f:
        cmaps=list(json.load(f))
     
-    for category in cmaps[1:]:
+    for category in cmaps:
         for gradienttype in category[1]:
-            x,y=2,2
-            Magneticfieldcalculator.draw_and_color_magnetic_lines(category[0],gradienttype,x,y,magnetic_outcomes)
-            plt.clf()
+            for x in range(2,8):
+                for y in range(2,8):
+                    Magneticfieldcalculator.draw_and_color_magnetic_lines(category[0],gradienttype,x,y,magnetic_outcomes)
+                    plt.clf()
 
 
 run_multi_universe()
